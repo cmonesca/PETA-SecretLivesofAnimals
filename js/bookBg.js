aed7066d879f429772d593cbf69e7e651c2d3c00
Carrie Ann <carriem@thecommunityus.com>
2019-03-21 20:38:38 -0400

    added some media queries

--- js/bookBg.js
+++ js/bookBg.js
@@ -5,7 +5,20 @@ $('#peta-book').bind('first', function() {
 
   console.log("page 1");
 
+	document.getElementById('#petaBookBg').addClass('visibile');
+
 	// document.getElementById('#petaBookBg').attr('background', 'none');
   //
   // document.getElementById('#soundBar').attr('left', '71%');
+});
+
+
+$('#peta-book').turn({
+  when: {
+    turning: function(event, page, pageObject) {
+      if(page===1) {
+        $('#peta-book').addClass('visible');
+      }
+    }
+  }
 });
